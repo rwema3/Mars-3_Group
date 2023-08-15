072f8716163667d9cb52faf22498b600329e837d
rwema3 <rwemadominik3@gmail.com>
2023-08-15 23:11:52 +0200

    CalssRecords Book

--- Python/order_management/books.py
+++ Python/order_management/books.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 import json
 from datetime import datetime
 
@@ -7,103 +5,43 @@ from typing import List
 from .sale import Sale
 
 class BookRecords:
-    """A record of all the sales made through the application.
-    
-    Attributes:
-        transactions: a list of the transactions
-    """
     def __init__(self, transactions: List[Sale]) -> None:
         self.transactions = transactions
     
     def __str__(self) -> str:
-        """Returns a string representation of a record.
-        
-        Args:
-        
-        Returns: A string
-        """
+        output = ['|      # | Date               | Customer   | Medication | Quantity | Purchase Price | Prescription |']
+        output += [f'| {idx:>6} |{datetime.fromtimestamp(transaction.timestamp):%Y-%m-%d %H:%M:%S} | {transaction.customerID[:10]:<10} | {transaction.name[:10]:<10} | {transaction.quantity:>8} | {transaction.purchase_price:>14} | {transaction.prescriptionID if transaction.prescriptionID else "-":>12} |' for idx, transaction in enumerate(self.transactions)]
 
-        #TODO: In the format below, return a representation of the records
-        # |      # | Date                | Customer   | Medication | Quantity | Purchase Price | Prescription |
-        # |      1 | 2023-06-03 21:23:25 | doe        | Quinine    |        3 |       1400 RWF | PHA1         |
+        return '\n'.join(output)
     
     def reportOnPrescriptions(self) -> str:
-        """Reports on prescription sales.
-
-        Args: 
-
-        Returns: A string report of the prescriptions processed
-        """
-        #TODO: From the transactions data, retrieve for each prescription, the actual medications that were processed
-        # and aggregate for each, the corresponding total price.
-
-        #TODO: output in the following format, the results: 
-        # |    # | Prescription ID | Total Price |
+        summary = {}
+        for transaction in self.transactions:
+            if transaction.prescriptionID is not None:
+                summary[transaction.prescriptionID] = summary.get(transaction.prescriptionID, 0) + transaction.purchase_price
+        output = ['|    # | Prescription ID | Total Price |']
+        output += [f'| {idx:>4} | {prescription:<15} | {total:>11}' for idx, prescription in enumerate(sorted(summary, key=lambda x: summary.get(x), reverse=True)) if (total := summary[prescription])]
+        return '\n'.join(output)        
 
     def purchasesByUser(self, customerID: str):
-        """Reports on the sales performed by a customer.
-        
-        Args:
-            customerID: Username of the customer.
-
-        Returns: A string representation of the corresponding transactions
-            
-        """
-        #TODO: Query the transactions to the `transactions` list below
-        transactions = None
-
-        return BookRecords(transactions).__str__()
+        return BookRecords([transaction for transaction in self.transactions if transaction.customerID == customerID]).__str__()
 
     def salesByAgent(self, salesperson: str):
-        """Reports on the sales performed by a pharmacist.
-        
-        Args:
-            salesperson: Username of the pharmacist.
-
-        Returns: A string representation of the corresponding transactions
-            
-        """
-        #TODO: Query the transactions to the `transactions` list below
-        transactions = None
-
-        # return the string representation
-        return BookRecords(transactions).__str__()
+        return BookRecords([transaction for transaction in self.transactions if transaction.salesperson == salesperson]).__str__()
     
-    def topNSales(self, start: datetime = datetime.strptime('1970-01-02', '%Y-%m-%d'), end: datetime = datetime.now(), n = 10) -> str:
-        """Return the top n sales ordered by the total price of purchases.
-
-        Args:
-            start: a datetime representing the start period to consider (datetime, default to 01 Jan 1970)
-            end: a datetime representing the end period to consider (datetime, default to current timestamp)
-            n: number of records to consider (int, default to 10)
-
-        Returns:
-        A string representation of the top n 
-        """
-        #TODO: Query the top transactions and save them to the variable `transactions` below
-        transactions = None
-
-        # return the string representation of the transactions.
+    def topNSales(self, start: datetime = datetime.strptime('1970-01-01', '%Y-%m-%d'), end: datetime = datetime.now(), n = 10) -> str:
+        ## by default, returns the top 10 sales in the books
+        transactions = sorted(
+            [transaction for transaction in self.transactions if transaction.timestamp >= start.timestamp() and transaction.timestamp <= end.timestamp()],
+            key=lambda x: x.purchase_price, reverse=True)[:n]
         return BookRecords(transactions).__str__()
 
 
     def totalTransactions(self) -> float:
-        """Returns the total cost of the transactions considered.
-        
-        Args:
-        
-        Returns: A floating number representing the total price
-        """
         return sum([transaction.purchase_price for transaction in self.transactions])
     
     @classmethod
-    def load(cls, infile: str) -> BookRecords:
-        """Loads a JSON file containing a number of sales object
-        
-        Args:
-            infile: path to the file to be read
-        Returns: A new object with the transactions in the file
-        """
-        #TODO: Implement the function. Make sure to handle the cases where
-        # the file does not exist.
-        return NotImplemented
+    def load(cls, infile: str):
+        with open(infile, 'r') as fin:
+            data = json.load(fin)
+        return cls([Sale(**el) for el in data])
