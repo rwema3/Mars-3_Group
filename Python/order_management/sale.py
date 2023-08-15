3507bc35ce0c6ecde91ffe20f6e37131942a0782
rwema3 <rwemadominik3@gmail.com>
2023-08-15 23:16:12 +0200

    Sales

--- Python/order_management/sale.py
+++ Python/order_management/sale.py
@@ -1,17 +1,6 @@
-class Sale:
-    """Class repreenting a sale that was successfully committed to the records.
+import datetime
 
-    Attributes:
-        id: unique identifier of the product sold (string)
-        name: name of the product sold
-        quantity: the quantity sold during the sale
-        price: unit price of the product
-        purchase_price: the total price at which the product was sold
-        timestamp: the UNIX timestamp of the sale
-        customerID: username of the buying customer (string)
-        salesperson: username of the pharmacist making the sale (string)
-        prescriptionID: identifier of the prescription used in the sale
-    """
+class Sale:
     def __init__(self, id: str, name: str, quantity: int, 
                  price: float, purchase_price: float, timestamp: float, 
                  customerID: str, salesperson: str, prescriptionID: str) -> None:
@@ -26,13 +15,5 @@ class Sale:
         self.prescriptionID = prescriptionID
 
     def __str__(self) -> str:
-        """Returns a string representation of a Sale object.
-
-        Args: None
-
-        Returns: A string
-        """
-        #TODO: Return a string that shows the product sold, its unit price
-        # the quantity, timestamp, and the total cost in a nice way.
         pass
 
