42ee5468979f823fd766e59f88013eba42e3594f
rwema3 <rwemadominik3@gmail.com>
2023-08-15 23:14:38 +0200

    For Prescrition

--- Python/order_management/prescription.py
+++ Python/order_management/prescription.py
@@ -5,15 +5,6 @@ from .product import Product
 
 
 class Prescription:
-    """Represents a prescription object
-
-    Attributes:
-        DoctorName: the name of the doctor who gave the prescription
-        PrescriptionID: ID of the prescription
-        Medications: list of the medications, this is the quantity, the ID, the name, and whether it was processed or not
-        # see format in prescriptions.json
-        CustomerID: ID of the customer
-    """
     def __init__(self, DoctorName: str, PrescriptionID: str, Medications: List[Dict[str, Union[int, str, bool]]],
                  CustomerID: str, Date: str) -> None:
         self.DoctorName = DoctorName
@@ -21,54 +12,30 @@ class Prescription:
         self.Medications = Medications
         self.CustomerID = CustomerID
 
-    def medecineInPrescription(self, product: Product, quantity: int) -> bool:
-        """Verifies if a medecine with the specified quantity is included in a prescription
-
-        Args:
-            product: the product to verify
-            quantity: the quantity to be added
-
-        Returns: A boolean denoting whether the value was found
-        """
-        #TODO: Implement the function
-        # 
-        return NotImplemented
-        
-    def markComplete(self, product: Product):
-        """Mark a product's sale complete in the prescriptions file
-
-        Args:
-            product: the product sold
-
-        Returns: None
-        """
-        #TODO: Change the value "ProcessedStatus" of the relevant product to True
-
-        return NotImplemented
+    def medecineInPrescription(self, product: Product, quantity: int):
+        return any([not el['ProcessedStatus'] and el['id'] == product.code and el['quantity'] == quantity for el in self.Medications])
+    
+    def markComplete(self, product: Product, quantity: int):
+        for med in self.Medications:
+            if product.code == med['id']:
+                med['ProcessedStatus'] = True
 
     def dump(self, outfile: str):
-        """Dumps the updated prescription to the specified file
-        
-        Args: 
-            outfile: path to the file where the output should be written
+        with open(outfile, 'r') as fin:
+            data = json.load(fin)
+        for el in data:
+            if el['PrescriptionID'] == self.PrescriptionID:
+                el['Medications'] = self.Medications
 
-        Returns: None
-        """
-        #TODO: Read the output file (safely). 
-
-        #TODO: Update the prescription that is being edited in the loaded data
-
-        #TODO: Save the updated object
+        with open(outfile, 'w') as fout:
+            json.dump(data, fout)
     
     @classmethod
-    def get(cls, infile: str, id: str):
-        """Retrieves a specific prescription from a file
-        
-        Args:
-            infile: path to the input file
-            id: identifier of the prescription to add
-
-        Returns: A prescription object as a dictionary
-        """
-        #TODO: Load the file and find the object with the relevant ID
-        #TODO: Return the relevant prescription
+    def load(cls, infile: str, id: str):
+        with open(infile, 'r') as fin:
+            data = json.load(fin)
+        prescriptions = {vals['PrescriptionID']: cls(**vals) for vals in data}
+        if id in prescriptions:
+            return prescriptions[id]
+        else:
+            return None
