93d4c0066d2f4abfa1c523f2cc168da8c075df57
rwema3 <rwemadominik3@gmail.com>
2023-08-15 23:10:06 +0200

    Impots py

--- Python/order_management/__init__.py
+++ Python/order_management/__init__.py
@@ -1,7 +1,12 @@
-from .stock import Stock, Product
+from .stock import Stock
+from .product import Product
 from .cart import Cart
-from .wrapper import Wrapper
-from .prescription import Prescription
+# from sale import Sale 
+from .stock import Stock
 from .books import BookRecords, Sale
 from .user_management import UserManagement, User
-from .menu import Menu
+from .menu import Menu 
+from .wrapper import Wrapper
+from .prescription import Prescription
+from .sale import Sale
+from .books import BookRecords
