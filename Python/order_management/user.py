94a8f7fc88d8b4ea8e965765c4bb468e67698dbe
rwema3 <rwemadominik3@gmail.com>
2023-08-16 15:54:13 +0200

    Pulled (for python)

--- Python/order_management/user.py
+++
@@ -1,8 +0,0 @@
-#TODO: Nothing -:
-
-class User:
-    def __init__(self, username: str, fullname: str, role: str, logged_in: bool) -> None:
-        self.username = username
-        self.fullname = fullname
-        self.role = role
-        self.logged_in = logged_in
